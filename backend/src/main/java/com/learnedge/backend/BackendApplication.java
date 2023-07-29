446755117db9f86305dc7615ab474c3df6ed9737
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-07-29 14:47:15 +0530

    Update BackendApplication.java

--- backend/src/main/java/com/learnedge/backend/BackendApplication.java
+++ backend/src/main/java/com/learnedge/backend/BackendApplication.java
@@ -2,15 +2,12 @@ package com.learnedge.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BackendApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 
 }
