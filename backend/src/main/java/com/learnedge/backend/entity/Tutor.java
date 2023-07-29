2238f5d135fc481cd6ecfa3f5db59586f8b3f1b8
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-07-29 14:23:29 +0530

    Add lombok, Remove getters, setters, constructors

--- backend/src/main/java/com/learnedge/backend/entity/Tutor.java
+++ backend/src/main/java/com/learnedge/backend/entity/Tutor.java
@@ -4,7 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Tutor {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -15,56 +24,5 @@ public class Tutor {
     private String tutorEmail;
     private String phoneNumber;
 
-    //Getters and Setters
-    public Long getTutorId() {
-        return tutorId;
-    }
 
-    public void setTutorId(Long tutorId) {
-        this.tutorId = tutorId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getTutorName() {
-        return tutorName;
-    }
-
-    public void setTutorName(String tutorName) {
-        this.tutorName = tutorName;
-    }
-
-    public String getTutorEmail() {
-        return tutorEmail;
-    }
-
-    public void setTutorEmail(String tutorEmail) {
-        this.tutorEmail = tutorEmail;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    //Constructor
-    public Tutor(Long tutorId, String password, String tutorName, String tutorEmail, String phoneNumber) {
-        this.tutorId = tutorId;
-        this.password = password;
-        this.tutorName = tutorName;
-        this.tutorEmail = tutorEmail;
-        this.phoneNumber = phoneNumber;
-    }
-    public Tutor(){
-
-    }
 }
--- backend/src/main/java/com/learnedge/backend/entity/User.java
+++ backend/src/main/java/com/learnedge/backend/entity/User.java
@@ -4,9 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -17,86 +25,8 @@ public class User {
     private String lastName;
     private String userEmail;
     private String phoneNumber;
-    private Date dateRegistered;
-    private Date dateOfBirth;
+    private Date dateRegistered; //JSON Date Format: YYYY-MM-DD
+    private Date dateOfBirth; //JSON Date Format: YYYY-MM-DD
 
-    //Getters and Setters
-    public Long getUserId() {
-        return userId;
-    }
 
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String studentEmail) {
-        this.userEmail = studentEmail;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Date getDateRegistered() {
-        return dateRegistered;
-    }
-
-    public void setDateRegistered(Date dateRegistered) {
-        this.dateRegistered = dateRegistered;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    //Constructor
-    public User(Long userId, String password, String firstName, String lastName, String userEmail, String phoneNumber, Date dateRegistered, Date dateOfBirth) {
-        this.userId = userId;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userEmail = userEmail;
-        this.phoneNumber = phoneNumber;
-        this.dateRegistered = dateRegistered;   //JSON Format: 2023-07-29
-        this.dateOfBirth = dateOfBirth;         //JSON Format: YYYY-MM-DD
-    }
-    public User() {
-
-    }
 }
