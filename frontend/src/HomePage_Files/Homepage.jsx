0b1233ccb09ca1193176dd31f53cbc1268b000f6
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-07-30 21:33:44 +0530

    Connected Backend(Course Details) to Homepage

--- frontend/src/HomePage_Files/Homepage.jsx
+++ frontend/src/HomePage_Files/Homepage.jsx
@@ -1,10 +1,45 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Homepage() {
+
+    const [courses, setCourses] = useState([])
+
+    useEffect(() => {
+        loadCourses();
+    }, [])
+
+    const loadCourses = async () => {
+        const listOfCourses = await axios.get("http://localhost:9081/course/getcourses")
+        //console.log(listOfCourses.data)
+        setCourses(listOfCourses.data)
+    }
 
     return (
         <>
             <h1 className="text-3xl font-bold ">Homepage Under construction</h1>
+            <h2 className="text-2xl font-bold ">List Of Courses:</h2>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Course Id</th>
+                        <th>Course Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {courses.map((e) => {
+                        return (
+                            <tr key={e.courseId}>
+                                <td>{e.courseId}</td>
+                                <td>{e.courseName}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table >
         </>
     )
 }
