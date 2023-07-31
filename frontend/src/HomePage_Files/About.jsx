9f8751fbdd99d5f2ca1d75f9af08c69ad674801f
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-08-01 01:26:08 +0530

    Update About.jsx

--- frontend/src/HomePage_Files/About.jsx
+++ frontend/src/HomePage_Files/About.jsx
@@ -7,23 +7,31 @@ export default function About() {
     return (
         <>
             <HomepageNavbar />
-            <div className='pb-20 p-5 select-none'>
+            <div className='px-10 pt-6 pb-20 select-none'>
                 {/* content goes inside this div */}
-                <h1 className='text-2xl'>About Content</h1>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+                <h1 className='text-4xl text-center pb-4'><b >About Us</b></h1>
+                <p className='text-center text-xl '>
+                    Welcome to our online course offering platform, a dynamic virtual space where learning knows no boundaries.
+                    We are passionate about empowering individuals from all walks of life to pursue their dreams, expand their horizons,
+                    and acquire valuable skills. Just like the world-renowned platform Coursera, we strive to create an inclusive learning
+                    environment that connects learners with the best universities and industry experts from around the globe. Our mission
+                    is to democratize education by providing accessible, high-quality courses that cater to a diverse range of interests
+                    and aspirations. With a vast and ever-expanding catalog of courses spanning various disciplines, we aim to meet the needs
+                    of lifelong learners, professionals seeking career advancement, and those simply driven by a thirst for knowledge. We pride
+                    ourselves on partnering with prestigious educational institutions and esteemed instructors who are at the forefront
+                    of their respective fields. This ensures that our learners receive unparalleled expertise and insights, enabling them
+                    to gain practical skills and stay relevant in an ever-evolving world. Flexibility is at the core of our platform.
+                    Whether you're a busy professional, a student, or someone with a hectic schedule, our courses are designed to
+                    accommodate your lifestyle. With the convenience of online learning, you can access course materials at any time,
+                    from anywhere, and learn at your own pace. We believe in the power of community and fostering connections.
+                    Our platform offers opportunities to engage with fellow learners, instructors, and industry professionals
+                    through discussion forums, virtual meetups, and networking events. Collaboration and interaction enrich the
+                    learning experience, allowing for the exchange of ideas and the cultivation of valuable connections that
+                    extend beyond the virtual classroom. Join us on this educational journey, where knowledge knows no boundaries
+                    and the possibilities for personal and professional growth are limitless. Together, let's embrace the
+                    transformative power of learning and shape a brighter future, one course at a time.
+                </p>
 
             </div>
             <Footer />
