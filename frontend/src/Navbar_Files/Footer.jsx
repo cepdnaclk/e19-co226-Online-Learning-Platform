74cb6ae016a836ce4f1c9196f55aa4f24784f0c8
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-07-31 15:17:48 +0530

    Update Footer.jsx

--- frontend/src/Navbar_Files/Footer.jsx
+++ frontend/src/Navbar_Files/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white py-4">
+        <footer className="bg-gray-800 text-white py-3 relative bottom-0 left-0 ">
             <div className="container mx-auto text-center">
                 <p>&copy; {new Date().getFullYear()} LearnEdge. All rights reserved.</p>
                 <p><i>Learn Anytime Anywhere </i> 🎓🕜🌐</p>
