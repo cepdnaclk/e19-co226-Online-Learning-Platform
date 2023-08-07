366ac3e8e3b62848728100c7a8401700aa7a318c
PumuduRajakaruna <e19306@eng.pdn.ac.lk>
2023-08-07 23:36:37 +0530

    Coures_Files were added

---
+++ frontend/src/Course_Files/Course.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function About() {
+    return (
+       <>
+          <Navbar />
+          <h1> Data Analytics Professional Certificate</h1>
+          <p>This is your path to a career in data analytics.
+             In this program, you`ll learn in-demand skills that will have you job-ready in less than 6 months. 
+             No degree or experience required.</p>
+
+          <h3>What you'll learn</h3>
+          <ul>
+            <li>Learn firsthand the procedures and methods that a junior or associate data analyst employs on a daily basis.</li>  
+            <li>Learn how to prepare and organize data for analysis, and how to use spreadsheets, SQL, and R programming to finish analysis and computations.</li>
+            <li>Learn the fundamental tools and techniques of analysis, such as spreadsheets, SQL, R programming, and Tableau.</li>
+            <li>Learn how to display data insights in dashboards, presentations, and popular visualization platforms.</li>
+          </ul> 
+
+          <h3>Skills you'll gain</h3>
+          <ul>
+            <li>Data Analysis</li>
+            <li>Data Visualization (DataViz)</li>
+            <li>case study</li>
+            <li>Spreadsheet</li>
+            <li>Data Collection</li>
+            <li>SQL</li>
+          </ul>
+
+          <Footer />
+       </>
+
+    )
+}
