b8a505559441378f9cc9eb1bcb3f02accba36b2f
PumuduRajakaruna <e19306@eng.pdn.ac.lk>
2023-08-09 00:54:55 +0530

    finish machine learning course page

--- frontend/src/Course_Files/Course.jsx
+++ frontend/src/Course_Files/Course.jsx
@@ -4,12 +4,12 @@ import Footer from "../Navbar_Files/Footer";
 function Course() {
    return (
       <>
-      <div className='pb-20 p-5 select-none'>
-         <div className='bg-blue-50 p-8 rounded-lg shadow-md h-1/2 flex w-full'>
+      <div className='pb-20 py-5 select-none'>
+         <div className='bg-blue-100 p-8 rounded-lg shadow-md h-1/2 flex w-full'>
            <div class=" h-full w-1/2"> 
-             <h1 className='text-4xl bold'> Data Analytics Professional Certificate</h1>
+             <h1 className='text-4xl font-semibold'> Data Analytics Professional Certificate</h1>
                 <div class='p-5'>
-                <p class='py-5'>This is your path to a career in data analytics. <br></br>
+                <p class='py-5 !pb-10'>This is your path to a career in data analytics. <br></br>
                 In this program, you`ll learn in-demand skills that will have you job-ready in less than 6 months.<br></br>
                 No degree or experience required.<br></br>
                 </p>
@@ -20,30 +20,35 @@ function Course() {
                 >
                 Enroll
                </button>
+               <div class='font-extralight pl-1 pt-1'>12 already enrolled</div>
 
                 </div>
            </div>
-           <div class="bg-blue-200 p-4 rounded-md h-full w-1/2 flex-grow">
-             <div class='text-xl bold'>
+           <div class="bg-blue-200 p-8 rounded-md h-full w-1/2 flex-grow shadow-lg">
+             <div class='text-xl font-medium'>
                Intermediate level
              </div>
-             <div class='text-l'>Recommended experience</div>
-             <div class='text-m'>A working knowledge of Python and Data Analysis and Visualization techniques. A minimum of high school math.    </div>
+             <div class='text-xl font-medium pt-3'>Recommended experience</div>
+             <div class='text-s pl-3 font-extralight pt-0'>A working knowledge of Python and Data Analysis and Visualization techniques.<br/> A minimum of high school math.</div>
+             <div class='text-xl font-medium pt-3'>12 hours (approximately)</div>
+             <div class='text-xl font-medium pt-3'>Flexible schedule</div>
+             <div class='text-s pl-3 font-extralight pt-0'>Learn at your own pace</div>
+             <div class='text-xl font-medium pt-3'>Progress towards a degree</div>
            </div>
 
          </div>
          
          <div class='h-1/2 pl-10 py-8'>
          <h3  class='text-2xl bold py-3'>What you'll learn</h3>
-         <ul>
+         <ul class='pl-8 font-extralight list-disc'>
             <li>Learn firsthand the procedures and methods that a junior or associate data analyst employs on a daily basis.</li>
             <li>Learn how to prepare and organize data for analysis, and how to use spreadsheets, SQL, and R programming to finish analysis and computations.</li>
             <li>Learn the fundamental tools and techniques of analysis, such as spreadsheets, SQL, R programming, and Tableau.</li>
             <li>Learn how to display data insights in dashboards, presentations, and popular visualization platforms.</li>
          </ul>
 
-         <h3 class='text-2xl bold py-3'>Skills you'll gain</h3>
-         <ul>
+         <h3  class='text-2xl bold py-3'>Skills you'll gain</h3>
+         <ul class='pl-8 font-extralight list-disc'>
             <li>Data Analysis</li>
             <li>Data Visualization (DataViz)</li>
             <li>case study</li>
