3149e3c8afe50f878d8f5763c436887c2717d8e9
Lakshitha Edirisinghe <e19095@eng.pdn.ac.lk>
2023-08-13 21:13:40 +0530

    Change Login Background colours

--- frontend/src/Login_Files/LoginForm.jsx
+++ frontend/src/Login_Files/LoginForm.jsx
@@ -43,7 +43,7 @@ function LoginForm() {
     };
 
     return (
-        <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        <div className="min-h-screen bg-gradient-to-br from-gray-500 via-black to-gray-500 py-6 flex flex-col justify-center sm:py-12">
             <div className="relative py-3 sm:max-w-xl sm:mx-auto">
                 <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
                 <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
